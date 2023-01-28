4d517eb1d10b4721f7a58e2171881fe249cb541d
Prasenjit Das <prasanjitacademic@gmail.com>
2023-01-28 11:45:41 +0530

    Intersection Observer Part 1

---
+++ Intersection Observer/app.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll('.card');
+
+
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        entry.target.classList.toggle('show',entry.isIntersecting)
+        // if(entry.isIntersecting){
+        //     observer.unobserve(entry.target)
+        // }
+    })
+
+
+})
+
+
+cards.forEach(card=>{
+    observer.observe(card);
+});
